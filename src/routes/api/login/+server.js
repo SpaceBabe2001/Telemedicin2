85aa0d60c82a1fa4119f5ac3aa55f32c7b2fcb4e
SpaceBabe2001 <125360025+SpaceBabe2001@users.noreply.github.com>
2024-05-05 17:59:58 +0200

    .

--- src/routes/api/login/+server.js
+++ src/routes/api/login/+server.js
@@ -13,12 +13,12 @@ export async function POST({ request, cookies }) {
 	if (!user) {
 		return new Response('Unauthorized', { status: 401 });
 	}
-	const match = bcrypt.compareSync(password, user.hashPass);
-	if (match) {
-		const token = jwt.sign({ id: user.id, email: user.email }, env.JWT_SECRET);
-		cookies.set('token', token, { httpOnly: true, path: '/' }); // cookies
-		return new Response('Logged in', { status: 200 });
-	} else {
-		return new Response('Unauthorized', { status: 401 });
-	}
+	// const match = bcrypt.compareSync(password, user.hashPass);
+	// if (match) {
+	// 	const token = jwt.sign({ id: user.id, email: user.email }, env.JWT_SECRET);
+	// 	cookies.set('token', token, { httpOnly: true, path: '/' }); // cookies
+	// 	return new Response('Logged in', { status: 200 });
+	// } else {
+	// 	return new Response('Unauthorized', { status: 401 });
+	// }
 }
