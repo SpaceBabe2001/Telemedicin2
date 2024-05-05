a8703bd815356f1581e063e3e6bb408aaeb9553a
SpaceBabe2001 <125360025+SpaceBabe2001@users.noreply.github.com>
2024-05-05 18:36:19 +0200

    .

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -7,19 +7,19 @@ export async function handle({ event, resolve }) {
 	console.log(pathname);
 	if (['/','/login', '/api/login/'].includes(pathname)) {
 		return await resolve(event);
-	// } else {
-	// 	if (!cookies.get('token')) {
-	// 		return new Response('Unauthorized', { status: 401 });
-	// 	} else {
-	// 		try {
-	// 			const token = jwt.verify(cookies.get('token'), env.JWT_SECRET);
-	// 			event.request.user = token;
-	// 		} catch (e) {
-	// 			console.log(e);
-	// 			return new Response('Unauthorized', { status: 401 });
-	// 		}
-	// 	}
+	} else {
+		if (!cookies.get('token')) {
+			return new Response('Unauthorized', { status: 401 });
+		} else {
+			try {
+				const token = jwt.verify(cookies.get('token'), env.JWT_SECRET);
+				event.request.user = token;
+			} catch (e) {
+				console.log(e);
+				return new Response('Unauthorized', { status: 401 });
+			}
+		}
 
-	// 	return await resolve(event);
+		return await resolve(event);
 	}
 }
